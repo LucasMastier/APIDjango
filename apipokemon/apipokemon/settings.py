da9faca696a216031e01a28c856fc633b96b8adc
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2024-05-12 00:59:15 +0200

    Connexion Django MySQL

--- apipokemon/apipokemon/settings.py
+++ apipokemon/apipokemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -75,13 +77,18 @@ WSGI_APPLICATION = 'apipokemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pokemon_schema',
+        'USER': 'root',
+        'PASSWORD': 'ProRncLegion94',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     },
     'pokemon':{
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
 }
 
 
